3b363309f7be6f60260788d08ae5a61743f46df0
Fazlay rabbi <rfazlay21@gmail.com>
2022-03-02 20:11:52 +0600

    yarn lock deleted

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: 'http://localhost:3000',
   })
 );
 
@@ -14,28 +14,28 @@ app.use(express.json());
 // app.use(cors());
 
 let todolist = [
-  { id: 1, task: " Brush Your teeth" },
-  { id: 2, task: " make Your bed" },
-  { id: 3, task: " Take Your Break Fast bed" },
-  { id: 4, task: " Do Math Home Work" },
+  { id: 1, task: ' Brush Your teeth' },
+  { id: 2, task: ' make Your bed' },
+  { id: 3, task: ' Take Your Break Fast bed' },
+  { id: 4, task: ' Do Math Home Work' },
 ];
 
-app.get("/", async (req, res) => {
-  res.send("Server is Running");
+app.get('/', async (req, res) => {
+  res.send('Server is Running');
 });
-app.get("/todolist", (req, res) => {
+app.get('/todolist', (req, res) => {
   res.send(todolist);
 });
 
-app.post("/addtodo", async (req, res) => {
+app.post('/addtodo', async (req, res) => {
   const newtodo = req.body;
   console.log(req.body);
   todolist.push(newtodo);
   // console.log(todolist);
-  res.send("data ok");
+  res.send('data ok');
 });
 
-app.delete("/todolist/:id", (req, res) => {
+app.delete('/todolist/:id', (req, res) => {
   console.log(req.params.id);
   const deletList = todolist.filter((element) => element.id != req.params.id);
   todolist = deletList;
@@ -44,7 +44,7 @@ app.delete("/todolist/:id", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("Running Server at port", port);
+  console.log('Running Server at port', port);
 });
 
 // fetch('https://example.com/delete-item/' + id, {
